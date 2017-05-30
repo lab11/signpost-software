191a59597ecc4c69b3712cb4bd218e67ab4030e4
Pat Pannuto <pat.pannuto@gmail.com>
2017-05-30 17:33:11 -0400

    annotate functions to force return value checks

--- apps/libsignpost/at_command.h
+++ apps/libsignpost/at_command.h
@@ -6,10 +6,21 @@
 #define AT_ERROR -1
 #define AT_NO_RESPONSE -2
 
+__attribute__ ((warn_unused_result))
 int at_send(int console_num, const char* cmd);
+
+__attribute__ ((warn_unused_result))
 int at_send_buf(int console_num, uint8_t* buf, size_t len);
 
+
+__attribute__ ((warn_unused_result))
 int at_wait_for_response(int console_num, uint8_t max_tries);
+
+__attribute__ ((warn_unused_result))
 int at_wait_for_custom_response(int console_num, uint8_t max_tries, const char* rstring);
+
+__attribute__ ((warn_unused_result))
 int at_get_response(int console_num, uint8_t max_tries, uint8_t* buf, size_t max_len);
+
+__attribute__ ((warn_unused_result))
 int at_get_custom_response(int console_num, uint8_t max_tries, uint8_t* buf, size_t max_len, const char* rstring);
