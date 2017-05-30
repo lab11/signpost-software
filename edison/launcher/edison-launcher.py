4fb49400160e4fcb4ae4287ff9c01364544f1952
Josh Adkins <adkinsjd@umich.edu>
2017-05-29 22:47:27 -0700

    added a first pass launcher script

---
+++ edison/launcher/edison-launcher.py
@@ -0,0 +1,51 @@
+import sys
+import os
+import signbus
+
+from periphery import GPIO
+from signpost import EdisonApiClient
+import subprocess
+
+def main():
+
+    #declare the gpio interrupt that signals there
+    #is an RPC to service
+    pending_rpc_gpio = GPIO(12, "in")
+
+    while True:
+
+        print("Woke up!")
+        proclist = []
+        while True:
+
+            #check if we have a new RPC
+            is_rpc = pending_rpc_gpio.read()
+            if(is_rpc):
+                #if we do have a new RPC launch the process and add it to the list
+                rpc_buf = EdisonApiClient.readRPC()
+
+                #we should eventually make this secure and make user(s)
+                #for module(s) with very limited permissions
+                rpc_user = rpc_buf[0]
+                p = subprocess.Popen(rpc_buf[1:])
+                print("Process {} started with PID {}".format(rpc_buf[1:],p.pid()))
+                proclist.append((rpc_user,p))
+
+            #check if any of the process in the list have finished and remove them
+            for proc in proclist:
+                if proc[1].poll():
+                    #the process is done - pop it from the list
+                    print("Process PID {} finished, removing from list".format(proc[1].pid()))
+                    proclist.remove(proc)
+
+            #if all processes have finished break so that we can sleep
+            if not proclist:
+                break;
+
+        #We are done doing stuff - go back to sleep
+        print("Done servicing RPCs - going to sleep")
+        os.system("echo mem > /sys/power/state")
+
+
+if __name__ == "__main__":
+    main()
