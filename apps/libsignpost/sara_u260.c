ebe43ba213c96a4603af5e369f94fee471ca0fc1
Joshua Adkuins <adkins@berkeley.edu>
2017-06-02 11:37:19 -0400

    fixed up sarau260

--- apps/libsignpost/sara_u260.c
+++ apps/libsignpost/sara_u260.c
@@ -10,12 +10,15 @@
 #define SARA_CONSOLE 110
 
 int sara_u260_init(void) {
-    at_send(SARA_CONSOLE,"AT\r");
+    int ret = at_send(SARA_CONSOLE,"AT\r");
+    if (ret < 0) return SARA_U260_ERROR;
 
     for(volatile uint32_t i = 0; i < 15000; i++);
 
-    at_send(SARA_CONSOLE,"ATE0\r");
-    int ret = at_wait_for_response(SARA_CONSOLE,3);
+    ret = at_send(SARA_CONSOLE,"ATE0\r");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_wait_for_response(SARA_CONSOLE,3);
     if(ret >= AT_SUCCESS) {
         return SARA_U260_SUCCESS;
     } else {
@@ -24,8 +27,11 @@ int sara_u260_init(void) {
 }
 
 static int sara_u260_check_connection(void) {
-    at_send(SARA_CONSOLE, "AT+COPS?\r");
+    int ret = at_send(SARA_CONSOLE, "AT+COPS?\r");
+    if (ret < 0) return SARA_U260_ERROR;
 
+    //We just need to check how many bytes it will return
+    //This tells us if the operator field is populated (has service)
     uint8_t buf[50];
     int len = at_get_response(SARA_CONSOLE, 3, buf, 50);
 
@@ -46,21 +52,31 @@ static int sara_u260_setup_packet_switch(void) {
     }
 
     //do we already have a pack switch setup?
-    at_send(SARA_CONSOLE,"AT+UPSND=0,0\r");
+    ret = at_send(SARA_CONSOLE,"AT+UPSND=0,0\r");
+    if (ret < 0) return SARA_U260_ERROR;
+
     ret = at_wait_for_response(SARA_CONSOLE, 3);
     if(ret == AT_ERROR) {
         //no we need to set one up
 
         //set the apn for our network - it can be blank
-        at_send(SARA_CONSOLE, "AT+UPSD=0,1,\"\"\r");
-        at_wait_for_response(SARA_CONSOLE, 3);
+        ret = at_send(SARA_CONSOLE, "AT+UPSD=0,1,\"\"\r");
+        if (ret < 0) return SARA_U260_ERROR;
+
+        ret = at_wait_for_response(SARA_CONSOLE, 3);
+        if (ret < 0) return SARA_U260_ERROR;
 
         //request to connect
-        at_send(SARA_CONSOLE, "AT+UPSDA=0,3\r");
-        at_wait_for_response(SARA_CONSOLE, 3);
+        ret = at_send(SARA_CONSOLE, "AT+UPSDA=0,3\r");
+        if (ret < 0) return SARA_U260_ERROR;
+
+        ret = at_wait_for_response(SARA_CONSOLE, 3);
+        if (ret < 0) return SARA_U260_ERROR;
 
         //did it work
-        at_send(SARA_CONSOLE,"AT+UPSND=0,0\r");
+        ret = at_send(SARA_CONSOLE,"AT+UPSND=0,0\r");
+        if (ret < 0) return SARA_U260_ERROR;
+
         ret = at_wait_for_response(SARA_CONSOLE, 3);
 
         if(ret < 0) {
@@ -79,16 +95,14 @@ static int sara_u260_setup_packet_switch(void) {
 static int sara_u260_del_file(const char* fname) {
     int ret;
 
-    const char* begin = "AT+UDELFILE=\"";
-    ret = at_send_buf(SARA_CONSOLE, (const uint8_t*)begin, strlen(begin));
-    if (ret < 0) return ret;
+    ret = at_send(SARA_CONSOLE, "AT+DELFILE=\"");
+    if (ret < 0) return SARA_U260_ERROR;
 
     ret = at_send_buf(SARA_CONSOLE, fname, strlen(fname));
-    if (ret < 0) return ret;
+    if (ret < 0) return SARA_U260_ERROR;
 
-    const char* end = "\"\r";
-    ret = at_send_buf(SARA_CONSOLE, (const uint8_t*)end, strlen(begin));
-    if (ret < 0) return ret;
+    ret = at_send_buf(SARA_CONSOLE, "\"\r");
+    if (ret < 0) return SARA_U260_ERROR;
 
     ret = at_wait_for_response(SARA_CONSOLE,3);
     if(ret >= 0) {
@@ -100,10 +114,25 @@ static int sara_u260_del_file(const char* fname) {
 
 static int sara_u260_write_to_file(const char* fname, uint8_t* buf, size_t len) {
 
-    char* c = (char*)malloc(strlen(fname)*sizeof(char)+17);
-    int clen = sprintf(c, "AT+UDWNFILE=\"%s\",%d\r", fname, len);
-    at_send_buf(SARA_CONSOLE, c, clen);
-    at_wait_for_custom_response(SARA_CONSOLE,3,"\n>");
+    int ret = at_send(SARA_CONSOLE, "AT+DWNFILE=\"");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE, fname);
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE, "\",");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    char c[15];
+    int clen = snprintf(c,15,"%d",len);
+    ret = at_send_buf(SARA_CONSOLE, c, clen);
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE, "\r");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_wait_for_custom_response(SARA_CONSOLE,3,"\n>");
+    if (ret < 0) return SARA_U260_ERROR;
 
     //now send the buffer in chunks of 30
     for(size_t i = 0; i < len; i+=30) {
@@ -112,10 +141,6 @@ static int sara_u260_write_to_file(const char* fname, uint8_t* buf, size_t len) 
         } else {
             at_send_buf(SARA_CONSOLE, buf+i, len-i);
         }
-    }
-
-    if(c) {
-        free(c);
     }
 
     int ret = at_wait_for_response(SARA_CONSOLE,3);
@@ -147,21 +172,40 @@ int sara_u260_basic_http_post(const char* url, const char* path, uint8_t* buf, s
     }
 
     //setup http profile
-    at_send(SARA_CONSOLE,"AT+UHTTP=0\r");
-    at_wait_for_response(SARA_CONSOLE, 3);
+    ret = at_send(SARA_CONSOLE,"AT+UHTTP=0\r");
+    if (ret < 0) return SARA_U260_ERROR;
 
-    at_send(SARA_CONSOLE,"AT+UHTTP=0,1,\"");
-    at_send(SARA_CONSOLE,url);
-    at_send(SARA_CONSOLE,"\"\r");
-    at_wait_for_response(SARA_CONSOLE, 3);
+    ret = at_wait_for_response(SARA_CONSOLE, 3);
+    if (ret < 0) return SARA_U260_ERROR;
 
-    at_send(SARA_CONSOLE,"AT+UHTTP=0,5,80\r");
-    at_wait_for_response(SARA_CONSOLE, 3);
+    ret = at_send(SARA_CONSOLE,"AT+UHTTP=0,1,\"");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE,url);
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE,"\"\r");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_wait_for_response(SARA_CONSOLE, 3);
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE,"AT+UHTTP=0,5,80\r");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_wait_for_response(SARA_CONSOLE, 3);
+    if (ret < 0) return SARA_U260_ERROR;
 
     //now actually do the post
-    at_send(SARA_CONSOLE,"AT+UHTTPC=0,4,\"");
-    at_send(SARA_CONSOLE,path);
-    at_send(SARA_CONSOLE,"\",\"postresult.txt\",\"postdata.bin\",2\r");
+    ret = at_send(SARA_CONSOLE,"AT+UHTTPC=0,4,\"");
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE,path);
+    if (ret < 0) return SARA_U260_ERROR;
+
+    ret = at_send(SARA_CONSOLE,"\",\"postresult.txt\",\"postdata.bin\",2\r");
+    if (ret < 0) return SARA_U260_ERROR;
+
     ret = at_wait_for_response(SARA_CONSOLE, 3);
 
     return ret;
@@ -172,12 +216,26 @@ int sara_u260_get_post_response(uint8_t* buf, size_t max_len) {
 }
 
 int sara_u260_get_post_partial_response(uint8_t* buf, size_t offset, size_t max_len) {
+    
+    int ret = at_send(SARA_CONSOLE,"AT+URDBLOCK=\"postresult.txt\",");
+    if (ret < 0) return SARA_U260_ERROR;
+
+
+    
     char c[60];
-    sprintf(c, "AT+URDBLOCK=\"postresult.txt\",%d,%d\r",offset,max_len);
-    at_send_buf(SARA_CONSOLE,c,strlen(c));
+    snprintf(c,60,"%d,%d\r",offset,max_len);
+    ret = at_send_buf(SARA_CONSOLE,c,strlen(c));
+    if (ret < 0) return SARA_U260_ERROR;
+
+    //should return data plus some framing characters, so take the
+    //data length being returned and add some room
 
     int len = max_len+100;
     uint8_t* tbuf = (uint8_t*)malloc(max_len*sizeof(uint8_t)+100);
+    if(!tbuf) {
+        return SARA_U260_ERROR;
+    }
+
     int ret = at_get_response(SARA_CONSOLE,3,tbuf,len);
     len = ret;
 
