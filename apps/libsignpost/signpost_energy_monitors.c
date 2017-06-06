58a6eac2ad9f386fcbe7503773d0d859ce83a8dd
Josh Adkins <adkinsjd@umich.edu>
2017-06-05 22:03:16 -0700

    trying to update energy library
    this is probably going to break travis

--- apps/libsignpost/signpost_energy_monitors.c
+++ apps/libsignpost/signpost_energy_monitors.c
@@ -6,8 +6,10 @@
 static uint8_t module_num_to_selector_mask[8] = {0x4, 0x8, 0x10, 0, 0, 0x20, 0x40, 0x80};
 static uint8_t is_ltc2943 = 0;
 
-static int prescaler = 0;
-
+////////////////////////////////////////////////////////////
+//Initialization functions
+//Make sure to call the right one
+/////////////////////////////////////////////////////////
 void signpost_energy_init (void) {
     // configure each ltc with the correct prescaler
     for (int i = 0; i < 8; i++) {
@@ -16,7 +18,7 @@ void signpost_energy_init (void) {
     }
 
     is_ltc2943 = 0;
-    prescaler = POWER_MODULE_PRESCALER_LTC2941;
+
     // set all channels open for Alert Response
     i2c_selector_select_channels_sync(0xFF);
 }
@@ -29,60 +31,105 @@ void signpost_energy_init_ltc2943 (void) {
     }
 
     is_ltc2943 = 1;
-    prescaler = POWER_MODULE_PRESCALER_LTC2943;
+
     // set all channels open for Alert Response
     i2c_selector_select_channels_sync(0x1FF);
 
     //max17205_configure_pack_sync();
 }
 
-static int get_ltc_energy (int selector_mask) {
+////////////////////////////////////////////////
+// Static helper functions
+// ////////////////////////////////////////////
+static int get_ltc_energy_uah (int selector_mask, int rsense) {
 	// Select correct LTC2941
 	i2c_selector_select_channels_sync(selector_mask);
 
-	// Get charge
-	return ltc294x_get_charge_sync();
+    if(!is_ltc2943) {
+	    return ltc2941_convert_to_coulomb_uah(ltc2941_get_charge_sync(),rsense);
+    } else {
+	    return ltc2943_convert_to_coulomb_uah(ltc2943_get_charge_sync(),rsense);
+    }
 }
 
-static int get_ltc_current_ua (int selector_mask) {
+static int get_ltc_current_ua (int selector_mask, int rsense) {
+	// Select correct LTC2941
     i2c_selector_select_channels_sync(selector_mask);
 
-    return ltc294x_convert_to_current_ua(ltc294x_get_current_sync(), 17);
+    return ltc2943_convert_to_current_ua(ltc2943_get_current_sync(),rsense);
 }
 
+static void reset_ltc_energy (int selector_mask) {
+	// Select correct LTC2941
+    i2c_selector_select_channels_sync(selector_mask);
+
+	ltc2941_reset_charge_sync();
+}
+
+///////////////////////////////////////////////////
+// Energy Reset Functions
+// ////////////////////////////////////////////////
+void signpost_energy_reset_all_energy (void) {
+    signpost_energy_reset_controller_energy();
+    signpost_energy_reset_solar_energy();
+    signpost_energy_reset_linux_energy();
+    for(uint8_t i = 0; i < 8; i++) {
+        signpost_energy_reset_module_energy(i);
+    }
+}
+
+void signpost_energy_reset_controller_energy (void) {
+    reset_ltc_energy(0x1);
+}
+
+void signpost_energy_reset_module_energy (int module_num) {
+    if(module_num != 3 && module_num != 4) {
+        reset_ltc_energy(module_num_to_selector_mask[module_num]);
+    }
+}
+
+void signpost_energy_reset_solar_energy (void) {
+    reset_ltc_energy(0x100);
+}
+
+void signpost_energy_reset_linux_energy (void) {
+    reset_ltc_energy(0x2);
+}
+
+///////////////////////////////////////////////////
+// Raw coulomb counter read functions
+// ///////////////////////////////////////////////
 int signpost_energy_get_controller_energy (void) {
-	return get_ltc_energy(0x1);
+    return get_ltc_energy_uah(0x1,POWER_MODULE_RSENSE)*CONTROLLER_VOLTAGE;
 }
-
-int signpost_energy_get_controller_current_ua (void) {
-    return get_ltc_current_ua(0x1);
-}
-
 int signpost_energy_get_linux_energy (void) {
-	return get_ltc_energy(0x2);
+	return get_ltc_energy_uah(0x2,POWER_MODULE_RSENSE)*LINUX_VOLTAGE;
 }
-
-int signpost_energy_get_linux_current_ua (void) {
-    return get_ltc_current_ua(0x2);
-}
-
 int signpost_energy_get_module_energy (int module_num) {
-	return get_ltc_energy(module_num_to_selector_mask[module_num]);
+	return get_ltc_energy_uah(module_num_to_selector_mask[module_num],POWER_MODULE_RSENSE)*MODULE_VOLTAGE;
 }
 
-int signpost_energy_get_module_current_ua (int module_num) {
-    return get_ltc_current_ua(module_num_to_selector_mask[module_num]);
+////////////////////////////////////////////////////
+// Raw current read functions
+// /////////////////////////////////////////////////////
+
+int signpost_energy_get_controller_current (void) {
+    return get_ltc_current_ua(0x1, POWER_MODULE_RSENSE);
 }
 
-int signpost_energy_get_battery_voltage_mv (void) {
-    uint16_t voltage;
-    int16_t current;
-    max17205_read_voltage_current_sync(&voltage,&current);
-    float v = max17205_get_voltage_mV(voltage);
-    return (int)v;
+int signpost_energy_get_linux_current (void) {
+    return get_ltc_current_ua(0x2, POWER_MODULE_RSENSE);
 }
 
-int signpost_energy_get_battery_current_ua (void) {
+int signpost_energy_get_module_current (int module_num) {
+    return get_ltc_current_ua(module_num_to_selector_mask[module_num],POWER_MODULE_RSENSE);
+}
+
+int signpost_energy_get_solar_current (void) {
+    return get_ltc_current_ua(0x100,POWER_MODULE_SOLAR_RSENSE);
+}
+
+int signpost_energy_get_battery_current (void) {
     uint16_t voltage;
     int16_t current;
     max17205_read_voltage_current_sync(&voltage,&current);
@@ -90,12 +137,16 @@ int signpost_energy_get_battery_current_ua (void) {
     return (int)c;
 }
 
-int signpost_energy_get_battery_energy (void) {
+
+///////////////////////////////////////////////
+//More information about the battery
+//////////////////////////////////////////////
+int signpost_energy_get_battery_capacity (void) {
     uint16_t percent;
     uint16_t charge;
     uint16_t full;
     max17205_read_soc_sync(&percent, &charge, &full);
-    return max17205_get_capacity_uAh(charge);
+    return max17205_get_capacity_uAh(full)*BATTERY_VOLTAGE_NOM;
 }
 
 int signpost_energy_get_battery_percent (void) {
@@ -106,58 +157,30 @@ int signpost_energy_get_battery_percent (void) {
     return max17205_get_percentage_mP(percent);
 }
 
-int signpost_energy_get_battery_capacity (void) {
+int signpost_energy_get_battery_energy (void) {
     uint16_t percent;
     uint16_t charge;
     uint16_t full;
     max17205_read_soc_sync(&percent, &charge, &full);
-    return max17205_get_capacity_uAh(full);
+    return max17205_get_capacity_uAh(charge)*BATTERY_VOLTAGE_NOM;
 }
 
-int signpost_energy_get_solar_voltage_mv (void) {
+
+/////////////////////////////////////////////////////////////////////
+// These functions give you voltages for battery and solar
+// ///////////////////////////////////////////////////////////////////
+int signpost_energy_get_battery_voltage (void) {
+    uint16_t voltage;
+    int16_t current;
+    max17205_read_voltage_current_sync(&voltage,&current);
+    float v = max17205_get_voltage_mV(voltage);
+    return (int)v;
+}
+
+
+int signpost_energy_get_solar_voltage (void) {
     //selector #3 slot 1
     i2c_selector_select_channels_sync(0x100);
 
-    return ltc294x_convert_to_voltage_mv(ltc294x_get_voltage_sync());
+    return ltc2943_convert_to_voltage_mv(ltc2943_get_voltage_sync());
 }
-
-int signpost_energy_get_solar_current_ua (void) {
-    //selector #3 slot 1
-    i2c_selector_select_channels_sync(0x100);
-
-    return ltc294x_convert_to_current_ua(ltc294x_get_current_sync(), 50);
-}
-
-void signpost_energy_reset (void) {
-    for (int i = 0; i < 8; i++) {
-        i2c_selector_select_channels_sync(1<<i);
-
-        ltc294x_reset_charge_sync();
-    }
-
-    if(is_ltc2943 ==1) {
-        i2c_selector_select_channels_sync(0x100);
-
-        ltc294x_reset_charge_sync();
-    }
-}
-
-__attribute__((const))
-int signpost_ltc_to_uAh (int ltc_energy, int rsense) {
-    if(!is_ltc2943) {
-        // prescaler needs to be a power of two
-        uint8_t power = 0;
-        uint8_t M;
-        for(int i = 0; i < 8; i++) {
-            if ((1<<i) & prescaler)
-                power = i;
-        }
-        M = 1<<power;
-        // Note that decimal values will be floored
-        return ltc_energy * (((85*50)/rsense)*M)/128;
-
-    } else {
-        return (int)(ltc_energy * (float)(340.0)*(50.0/rsense)*(prescaler/4096.0));
-    }
-}
-
--- apps/libsignpost/signpost_energy_monitors.h
+++ apps/libsignpost/signpost_energy_monitors.h
@@ -5,30 +5,57 @@ extern "C" {
 #endif
 
 // qlsb = 0.0625mAh with 0.017 Ohm sense resistor
-#define POWER_MODULE_PRESCALER_LTC2941 32
-#define POWER_MODULE_PRESCALER_LTC2943 256
-// rsense = 0.017 Ohm
-#define POWER_MODULE_RSENSE 17
+#define POWER_MODULE_PRESCALER 32
+#define POWER_MODULE_PRESCALER_LTC2943 64
 
+//in Mohm
+#define POWER_MODULE_RSENSE 17
+#define POWER_MODULE_SOLAR_RSENSE 50
+
+//voltages for the energy numbers
+#define MODULE_VOLTAGE 5
+#define LINUX_VOLTAGE 5
+#define CONTROLLER_VOLTAGE 3.3
+#define BATTERY_VOLTAGE_NOM 11.1
+
+//initialize based on the version of the monitors that we are using
 void signpost_energy_init (void);
 void signpost_energy_init_ltc2943 (void);
 
-int signpost_energy_get_controller_energy (void);
-int signpost_energy_get_controller_current_ua (void);
-int signpost_energy_get_linux_energy (void);
-int signpost_energy_get_linux_current_ua (void);
-int signpost_energy_get_module_energy (int module_num);
-int signpost_energy_get_module_current_ua (int module_num);
-int signpost_energy_get_battery_voltage_mv (void);
-int signpost_energy_get_battery_current_ua (void);
-int signpost_energy_get_battery_energy (void);
-int signpost_energy_get_battery_percent (void);
-int signpost_energy_get_battery_capacity (void);
-int signpost_energy_get_solar_voltage_mv (void);
-int signpost_energy_get_solar_current_ua (void);
-void signpost_energy_reset (void);
+//this zeros the coulomb counters for each counter
+void signpost_energy_reset_all_energy (void);
+void signpost_energy_reset_controller_energy (void);
+void signpost_energy_reset_linux_energy (void);
+void signpost_energy_reset_solar_energy (void);
+void signpost_energy_reset_module_energy (int module_num);
 
-int signpost_ltc_to_uAh (int ltc_energy, int rsense);
+//these functions return the energy used in uWh by reading
+//the coulomb counters and multiplying by the voltage
+//Note that energy cannot flow from modules back into the battery
+//So we use unsigned energy types here
+uint32_t signpost_energy_get_controller_energy (void);
+uint32_t signpost_energy_get_linux_energy (void);
+uint32_t signpost_energy_get_module_energy (int module_num);
+
+//these function return the instantaneous current in uA
+//Battery current can be positive or negative
+int32_t signpost_energy_get_battery_current (void);
+uint32_t signpost_energy_get_solar_current (void);
+uint32_t signpost_energy_get_controller_current (void);
+uint32_t signpost_energy_get_linux_current (void);
+uint32_t signpost_energy_get_module_current (int module_num);
+
+//uWh
+uint32_t signpost_energy_get_battery_capacity (void);
+//whole number percent
+uint8_t signpost_energy_get_battery_percent (void);
+//uWh
+uint32_t signpost_energy_get_battery_energy (void);
+
+//these functions return instantaneous current for Voltage
+//These are returned in mV
+uint16_t signpost_energy_get_battery_voltage (void);
+uint16_t signpost_energy_get_solar_voltage (void);
 
 #ifdef __cplusplus
 }
