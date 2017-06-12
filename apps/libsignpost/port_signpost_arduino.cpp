cf4cd5accec79fdcf9f2f7498ca9bcac166a9a24
theomil <thmiller917@gmail.com>
2017-06-12 19:34:55 -0400

    Finished initial draft of port_signpost_arduino

--- apps/libsignpost/port_signpost_arduino.cpp
+++ apps/libsignpost/port_signpost_arduino.cpp
@@ -1,6 +1,8 @@
 #include "Arduino.h"
-#include <Wire.h>
+#include <SignWire.h>
 #include "port_signpost.h"
+
+//TODO: Verify/test multi-master support
 
 //TODO: choose mod in, mod out, and debug led pins for arduino
 #define ARDUINO_MOD_OUT
@@ -9,62 +11,72 @@
 #define SIGNPOST_I2C_SPEED 400000
 
 //TODO: Add error handling and error codes
-
-static void slave_listen_callback_helper(int num_bytes);
 static void mod_in_callback_helper();
+static void slave_listen_callback_helper(int num_bytes);
+static void slave_read_callback_helper();
+
+//TwoWire object used solely for Signpost i2c
+TwoWire SignWire();
 
 uint8_t g_arduino_i2c_address;
 port_signpost_callback g_mod_in_callback = NULL;
 port_signpost_callback g_slave_listen_callback = NULL;
 
 uint8_t* g_slave_receive_buf = NULL;
-size_t g_slave_receive_buf_max_size = 0;
+size_t g_slave_receive_buf_max_len = 0;
 size_t g_slave_receive_buf_len = 0;
 
+uint8_t* g_slave_transmit_buf = NULL;
+size_t g_slave_transmit_buf_len = 0;
+
 int port_signpost_init(uint8_t i2c_address) {
-	Wire.begin();
-	Wire.setClock(SIGNPOST_I2C_SPEED);
+	SignWire.begin();
+	SignWire.setClock(SIGNPOST_I2C_SPEED);
 	g_arduino_i2c_address = i2c_address;
 	pinMode(ARDUINO_MOD_IN, INPUT_PULLUP);
 	pinMode(ARDUINO_MOD_OUT, OUTPUT);
 	pinMode(DEBUG_LED, OUTPUT);
-	Wire.onReceive(slave_listen_callback_helper);
+	SignWire.onReceive(slave_listen_callback_helper);
 	return 0;
 }
 
 int port_signpost_i2c_master_write(uint8_t addr, uint8_t* buf, size_t len) {
-	Wire.begin();
-	Wire.setClock(SIGNPOST_I2C_SPEED);
-	Wire.beginTransmission(addr);
-	int num_written = Wire.write(buf, len);
-	Wire.endTransmission(stop);
+	SignWire.begin();
+	SignWire.setClock(SIGNPOST_I2C_SPEED);
+	SignWire.beginTransmission(addr);
+	int num_written = SignWire.write(buf, len);
+	SignWire.endTransmission(stop);
 	return num_written;
 }
 
 int port_signpost_i2c_slave_listen(port_signpost_callback cb, uint8_t* buf, size_t max_len) {
-	Wire.begin(g_arduino_i2c_address);
-	Wire.setClock(SIGNPOST_I2C_SPEED);
+	SignWire.begin(g_arduino_i2c_address);
+	SignWire.setClock(SIGNPOST_I2C_SPEED);
 	g_slave_receive_buf = buf;
-	g_slave_receive_buf_max_size = max_len;
+	g_slave_receive_buf_max_len = max_len;
 	g_slave_receive_buf_len = 0;
 	return 0;
 }
 
 int port_signpost_i2c_slave_read_setup(uint8_t* buf, size_t len) {
-
+	SignWire.begin(g_arduino_i2c_address);
+	SignWire.setClock(SIGNPOST_I2C_SPEED);
+	//Set global i2c transmit buffer to input buffer
+	g_slave_transmit_buf = buf;
+	g_slave_transmit_buf_len = len;
 }
 
-int port_signpost_mod_out_set(void) {
+int port_signpost_mod_out_set() {
 	digitalWrite(ARDUINO_MOD_OUT, HIGH);
 	return 0;
 }
 
-int port_signpost_mod_out_clear(void) {
+int port_signpost_mod_out_clear() {
 	digitalWrite(ARDUINO_MOD_OUT, LOW);
 	return 0;
 }
 
-int port_signpost_mod_in_read(void) {
+int port_signpost_mod_in_read() {
 	return digitalRead(ARDUINO_MOD_IN);
 }
 
@@ -79,27 +91,30 @@ int port_signpost_mod_in_disable_interrupt(void) {
 	return 0;
 }
 
+//TODO: Implement version that puts Arduino into a sleep state
 void port_signpost_wait_for(void* wait_on_true) {
-
+	while (!wait_on_true) {
+		continue;
+	}
 }
 
 void port_signpost_delay_ms(unsigned ms) {
 	delay(ms);
 }
 
-int port_signpost_debug_led_on(void) {
+int port_signpost_debug_led_on() {
 	digitalWrite(DEBUG_LED, HIGH);
 	return 0;
 }
 
-int port_signpost_debug_led_off(void) {
+int port_signpost_debug_led_off() {
 	digitalWrite(DEBUG_LED, LOW);
 	return 0;
 }
 
 static void mod_in_callback_helper() {
 	//If no callback is assigned, return
-	if (g_mod_in_callback == NULL) {
+	if (g_mod_in_callback == nullptr) {
 		return;
 	}
 	*(g_mod_in_callback) (0);
@@ -107,12 +122,23 @@ static void mod_in_callback_helper() {
 //TODO: Add condition for overflow of g_slave_receive_buf
 static void slave_listen_callback_helper(int num_bytes) {
 	//If no callback is assigned, return
-	if (g_slave_listen_callback == NULL) {
+	if (g_slave_listen_callback == nullptr) {
 		return;
 	}
-	//Transfer data from read buffer in Wire object to g_slave_receive_buf, then call custom callback
+	if (g_slave_receive_buf == nullptr) {
+		return;
+	}
+	//Transfer data from read buffer in SignWire object to g_slave_receive_buf, then call custom callback
 	for (uint i = 0; i < num_bytes && i < g_slave_receive_buf_max_len; ++i, ++g_slave_receive_buf_len) {
-		g_slave_receive_buf[i] = Wire.read();
+		g_slave_receive_buf[g_slave_receive_buf_len] = SignWire.read();
 	}
 	*(g_slave_listen_callback) (num_bytes);
+}
+
+static void slave_read_callback_helper() {
+	//If no buffer is assigned, return
+	if (g_slave_transmit_buf == nullptr) {
+		return;
+	}
+	SignWire.write(g_slave_transmit_buf, g_slave_transmit_buf_len);
 }
