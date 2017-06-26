ae4dada8e5a972d94665bd9ba28953a046f30225
Joshua Adkuins <adkins@berkeley.edu>
2017-06-26 16:52:22 -0700

    added the secure version of the cellular test
    This is not currently working - the SARA U260 is returning an "internal error"
    even when I copy their example. I filed a support ticket

---
+++ apps/radio_module/cellular_test_secure/README.md
@@ -0,0 +1,6 @@
+Sara U260 Cellular Module Test
+=============================
+
+This test uses the sara u260 library to perform an octetstream post
+to a post test server using https, then a get from a post test server
+using https.
---
+++ apps/radio_module/cellular_test_secure/main.c
@@ -0,0 +1,71 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "tock.h"
+#include "timer.h"
+#include "console.h"
+#include "led.h"
+#include "sara_u260.h"
+
+
+int main (void) {
+    printf("Starting Cell Test\n");
+
+    delay_ms(2000);
+
+    sara_u260_init();
+
+    delay_ms(1000);
+
+    uint8_t buf[5] = {'L','a','b','1','1'};
+    printf("Posting...\n");
+    int ret = sara_u260_basic_https_post("httpbin.org","/post",buf,5);
+
+    if(ret == SARA_U260_NO_SERVICE) {
+        printf("No Service! try again in a bit.\n");
+    }
+
+    delay_ms(40000);
+
+    if(ret >= 0) {
+        uint8_t dbuf[500] = {0};
+        printf("Getting response\n");
+        int l = sara_u260_get_post_response(dbuf, 499);
+        if(l < 0) {
+            printf("Did not get response\n");
+        } else {
+            dbuf[l] = 0;
+            printf("Got response of length %d: \n",l);
+            printf("%s\n", (char*)dbuf);
+        }
+    }
+
+    delay_ms(40000);
+
+    printf("Now trying http get\n");
+    ret = sara_u260_basic_https_get("httpbin.org","/get");
+
+    if(ret == SARA_U260_NO_SERVICE) {
+        printf("No Service! try again in a bit.\n");
+    }
+
+    delay_ms(40000);
+
+    if(ret >= 0) {
+        uint8_t dbuf[500] = {0};
+        printf("Getting response\n");
+        int l = sara_u260_get_get_response(dbuf, 499);
+        if(l < 0) {
+            printf("Did not get response\n");
+        } else {
+            dbuf[l] = 0;
+            printf("Got response of length %d: \n",l);
+            printf("%s\n", (char*)dbuf);
+        }
+    }
+}
