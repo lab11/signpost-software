3e9bcc527d05673c10d8a40d80ed82c7621c8cc3
theomil <thmiller917@gmail.com>
2017-07-06 17:26:30 -0400

    Fixed i2c slave receive issue
    i2c was never switched back to slave mode
    after transmitting a message as master. Fixed by adding
    call to Wire.begin(address) at the end of message.
    port_signpost_delay_ms now uses the interrupt friendly
    delayMicroseconds function instead of delay function.

--- apps/libsignpost/port_signpost_arduino.cpp
+++ apps/libsignpost/port_signpost_arduino.cpp
@@ -14,7 +14,13 @@
 #define ARDUINO_MOD_OUT 		5
 #define DEBUG_LED 				LED_BUILTIN
 #define SIGNPOST_I2C_SPEED 		400000
+<<<<<<< HEAD
 #define _PRINTF_BUFFER_LENGTH_	512
+=======
+#define _PRINTF_BUFFER_LENGTH_	64
+#define ARDUINO_DEBUG_1 6
+#define ARDUINO_DEBUG_2 7
+>>>>>>> Fixed i2c slave receive issue
 
 //enable/disable printing debug messages over uart
 #define ENABLE_ARDUINO_PORT_PRINTF
@@ -48,6 +54,8 @@ int port_signpost_init(uint8_t i2c_address) {
 	pinMode(ARDUINO_MOD_IN, INPUT);
 	pinMode(ARDUINO_MOD_OUT, OUTPUT);
 	pinMode(DEBUG_LED, OUTPUT);
+	pinMode(ARDUINO_DEBUG_1, OUTPUT);
+	pinMode(ARDUINO_DEBUG_2, OUTPUT);
 	Wire.onReceive(slave_listen_callback_helper);
 	Wire.onRequest(slave_read_callback_helper);
 	return 0;
@@ -58,18 +66,33 @@ int port_signpost_i2c_master_write(uint8_t addr, uint8_t* buf, size_t len) {
 		return -1;
 	}
 	Wire.begin();
+	digitalWrite(ARDUINO_DEBUG_1, HIGH);
+	digitalWrite(ARDUINO_DEBUG_2, LOW);
 	Wire.setClock(SIGNPOST_I2C_SPEED);
 	Wire.beginTransmission(addr);
 	int num_written = Wire.write(buf, len);
+<<<<<<< HEAD
 	int rc = Wire.endTransmission();
 	//Puts Arduino back into i2c slave mode
 	Wire.begin(g_arduino_i2c_address);
+=======
+	Wire.endTransmission();
+	Wire.begin(g_arduino_i2c_address);
+	digitalWrite(ARDUINO_DEBUG_1, HIGH);
+	digitalWrite(ARDUINO_DEBUG_2, LOW);
+>>>>>>> Fixed i2c slave receive issue
 	return num_written;
 }
 
 int port_signpost_i2c_slave_listen(port_signpost_callback cb, uint8_t* buf, size_t max_len) {
 	Wire.begin(g_arduino_i2c_address);
+<<<<<<< HEAD
 	g_slave_listen_callback = cb;
+=======
+	digitalWrite(ARDUINO_DEBUG_1, LOW);
+	digitalWrite(ARDUINO_DEBUG_2, HIGH);
+	Wire.setClock(SIGNPOST_I2C_SPEED);
+>>>>>>> Fixed i2c slave receive issue
 	g_slave_receive_buf = buf;
 	g_slave_receive_buf_max_len = max_len;
 	g_slave_receive_buf_len = 0;
@@ -78,6 +101,12 @@ int port_signpost_i2c_slave_listen(port_signpost_callback cb, uint8_t* buf, size
 //Function untested
 int port_signpost_i2c_slave_read_setup(uint8_t* buf, size_t len) {
 	Wire.begin(g_arduino_i2c_address);
+<<<<<<< HEAD
+=======
+	digitalWrite(ARDUINO_DEBUG_1, LOW);
+	digitalWrite(ARDUINO_DEBUG_2, HIGH);
+	Wire.setClock(SIGNPOST_I2C_SPEED);
+>>>>>>> Fixed i2c slave receive issue
 	//Set global i2c transmit buffer to input buffer
 	g_slave_transmit_buf = buf;
 	g_slave_transmit_buf_len = len;
@@ -117,12 +146,22 @@ void port_signpost_wait_for(bool* wait_on_true) {
 }
 
 void port_signpost_delay_ms(unsigned ms) {
+<<<<<<< HEAD
 	//For every 1 ms delay, call delayMicroseconds once.
 	//delayMicroseconds does not work for inputs > 16383,
 	//so a loop is used to divide up the duration.
 	for (unsigned i = 0; i < ms; ++i) {
 		delayMicroseconds(1000);
 	}
+=======
+	// For every 1 ms delay, call delayMicroseconds once.
+	// delayMicroseconds does not work for inputs > 16383,
+	// so a loop is used to divide up the duration.
+	for (unsigned i = 0; i < ms; ++i) {
+		delayMicroseconds(1000);
+	}
+	//delay(ms);
+>>>>>>> Fixed i2c slave receive issue
 }
 
 int port_signpost_debug_led_on() {
