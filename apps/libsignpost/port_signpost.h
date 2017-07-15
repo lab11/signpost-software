9fa8a2ea96ffa6b411d0dee89ad52562b3a9272f
Joshua Adkuins <adkins@berkeley.edu>
2017-07-15 15:27:15 -0700

    added the start of an update api - still need radio implementation
    merged the port functions

--- apps/libsignpost/port_signpost.h
+++ apps/libsignpost/port_signpost.h
@@ -139,6 +139,13 @@ int port_signpost_save_state(module_state_t* state);
  * */
 int port_signpost_load_state(module_state_t* state);
 
+//This write to microcontroller flash
+int port_signpost_flash_write(uint32_t address, uint8_t* data, uint32_t data_len);
+
+//This is used to perform an update
+//it should, check the crc, copy the flash to the destination, then reset
+int port_signpost_apply_update(uint32_t dest_address, uint32_t source_address, uint32_t update_length, uint16_t crc);
+
 #ifdef __cplusplus
 }
 #endif
