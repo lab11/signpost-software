6bc23488d40477a4c04ab7df04ff864e5edeebaf
Josh Adkins <adkinsjd@umich.edu>
2017-07-17 16:58:34 -0700

    get a better file

--- apps/radio_module/cellular_test/main.c
+++ apps/radio_module/cellular_test/main.c
@@ -48,7 +48,7 @@ int main (void) {
     delay_ms(10000);
 
     printf("Now trying http get\n");
-    ret = sara_u260_basic_http_get("httpbin.org","/get");
+    ret = sara_u260_basic_http_get("rawgit.com","/lab11/signpost-software/master/apps/radio_module/cellular_test/README.md");
 
     if(ret == SARA_U260_NO_SERVICE) {
         printf("No Service! try again in a bit.\n");
@@ -57,9 +57,9 @@ int main (void) {
     delay_ms(10000);
 
     if(ret >= 0) {
-        uint8_t dbuf[500] = {0};
+        uint8_t dbuf[1000] = {0};
         printf("Getting response\n");
-        int l = sara_u260_get_get_response(dbuf, 499);
+        int l = sara_u260_get_get_response(dbuf, 999);
         if(l < 0) {
             printf("Did not get response\n");
         } else {
