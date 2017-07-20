3415302afa0c4a3de8b127f9396b86886681e6e3
Josh Adkins <adkinsjd@umich.edu>
2017-07-20 03:23:06 -0700

    now sending energy data to radio

--- apps/controller/signpost_controller_send_bytes_app/main.c
+++ apps/controller/signpost_controller_send_bytes_app/main.c
@@ -21,16 +21,8 @@
 #include "signpost_energy_monitors.h"
 #include "signpost_controller.h"
 
-#include "bonus_timer.h"
-
-static void get_energy_remaining (void);
-static void get_energy_average (void);
-static void get_batsol (void);
-
 uint8_t gps_buf[20];
-uint8_t energy_buf[20];
-uint8_t energy_av_buf[20];
-uint8_t batsol_buf[20];
+uint8_t energy_buf[40];
 
 static void watchdog_tickler (int which) {
   static bool gps_tickle = false;
@@ -50,83 +42,47 @@ static void watchdog_tickler (int which) {
   }
 }
 
+static void send_gps_update (__attribute__((unused)) int now,
+                                __attribute__((unused)) int experation,
+                                __attribute__((unused)) int unused,
+                                __attribute__((unused)) void* ud) {
 
-static void get_energy_average (void) {
+  signpost_timelocation_time_t time;
+  signpost_timelocation_location_t location;
+  signpost_controller_get_gps(&time,&location);
 
-
-  //int c_power = signpost_energy_policy_get_controller_average_power()/1000;
-  int c_power = 0;
-  if(c_power <= 0) {
-      c_power = 0;
+  gps_buf[2] = time.day;
+  gps_buf[3] = time.month;
+  gps_buf[4] = time.year;
+  gps_buf[5] = time.hours;
+  gps_buf[6] = time.minutes;
+  gps_buf[7] = time.seconds;
+  gps_buf[8] = ((location.latitude & 0xFF000000) >> 24);
+  gps_buf[9] = ((location.latitude & 0x00FF0000) >> 16);
+  gps_buf[10] = ((location.latitude & 0x0000FF00) >> 8);
+  gps_buf[11] = ((location.latitude & 0x000000FF));
+  gps_buf[12] = ((location.longitude & 0xFF000000) >> 24);
+  gps_buf[13] = ((location.longitude & 0x00FF0000) >> 16);
+  gps_buf[14] = ((location.longitude & 0x0000FF00) >> 8);
+  gps_buf[15] = ((location.longitude & 0x000000FF));
+  if(time.satellite_count >= 3) {
+    gps_buf[16] = 0x02;
+  } else if (time.satellite_count >=4) {
+    gps_buf[16] = 0x03;
+  } else {
+    gps_buf[16] = 0x01;
   }
-
-  energy_av_buf[8] = (((c_power) & 0xFF00) >> 8 );
-  energy_av_buf[9] = (((c_power) & 0xFF));
-  energy_av_buf[10] = 0;
-  energy_av_buf[11] = 0;
-
-  //send this data to the radio module
-  for(uint8_t i = 0; i < 8; i++) {
-      if(i==3 || i == 4) continue;
-
-      //int mod_energy = signpost_energy_policy_get_module_average_power(i)/1000;
-      int mod_energy = 0;
-
-      if(mod_energy <= 0) {
-          mod_energy = 0;
-      }
-
-      energy_av_buf[2+i*2] = (((mod_energy) & 0xFF00) >> 8);
-      energy_av_buf[2+i*2+1] = (((mod_energy) & 0xFF));
-  }
-
-  int rc;
-  rc = signpost_networking_send_bytes(ModuleAddressRadio,energy_av_buf,18);
-  energy_av_buf[1]++;
-
-  if(rc >= 0) {
-    // Tickle the watchdog because something good happened.
-  }
-
-  watchdog_tickler(2);
-
+  gps_buf[17] = time.satellite_count;
+  int rc = signpost_networking_send_bytes(ModuleAddressRadio,gps_buf,18);
+  if(rc < 0) printf("Error sending GPS packet\n");
+  gps_buf[1]++;
 }
 
-static void get_energy_remaining (void) {
-  
+static void send_energy_update (__attribute__((unused)) int now,
+                                __attribute__((unused)) int experation,
+                                __attribute__((unused)) int unused,
+                                __attribute__((unused)) void* ud) {
 
-
-  //send this data to the radio module
-  /*energy_buf[2] = (((fram.energy.module_energy_remaining[0]/1000) & 0xFF00) >> 8 );
-  energy_buf[3] = (((fram.energy.module_energy_remaining[0]/1000) & 0xFF));
-  energy_buf[4] = (((fram.energy.module_energy_remaining[1]/1000) & 0xFF00) >> 8 );
-  energy_buf[5] = (((fram.energy.module_energy_remaining[1]/1000) & 0xFF));
-  energy_buf[6] = (((fram.energy.module_energy_remaining[2]/1000) & 0xFF00) >> 8 );
-  energy_buf[7] = (((fram.energy.module_energy_remaining[2]/1000) & 0xFF));
-  energy_buf[8] = (((fram.energy.controller_energy_remaining/1000) & 0xFF00) >> 8 );
-  energy_buf[9] = (((fram.energy.controller_energy_remaining/1000) & 0xFF));
-  energy_buf[10] = 0;
-  energy_buf[11] = 0;
-  energy_buf[12] = (((fram.energy.module_energy_remaining[5]/1000) & 0xFF00) >> 8 );
-  energy_buf[13] = (((fram.energy.module_energy_remaining[5]/1000) & 0xFF));
-  energy_buf[14] = (((fram.energy.module_energy_remaining[6]/1000) & 0xFF00) >> 8 );
-  energy_buf[15] = (((fram.energy.module_energy_remaining[6]/1000) & 0xFF));
-  energy_buf[16] = (((fram.energy.module_energy_remaining[7]/1000) & 0xFF00) >> 8 );
-  energy_buf[17] = (((fram.energy.module_energy_remaining[7]/1000) & 0xFF));*/
-
-  int rc;
-  rc = signpost_networking_send_bytes(ModuleAddressRadio,energy_buf,18);
-  energy_buf[1]++;
-
-  if(rc >= 0) {
-    // Tickle the watchdog because something good happened.
-  }
-
-  watchdog_tickler(2);
-
-}
-
-static void get_batsol (void) {
   int battery_voltage = signpost_energy_get_battery_voltage_mv();
   int battery_current = signpost_energy_get_battery_current_ua();
   int battery_energy = (int)((signpost_energy_get_battery_energy_uwh()/BATTERY_VOLTAGE_NOM)/1000.0);
@@ -140,27 +96,51 @@ static void get_batsol (void) {
   printf("\tSolar Voltage (mV): %d\tcurrent (uA): %d\n",solar_voltage,solar_current);
   printf("/**************************************/\n");
 
-  batsol_buf[2] = ((battery_voltage & 0xFF00) >> 8);
-  batsol_buf[3] = ((battery_voltage & 0xFF));
-  batsol_buf[4] = ((battery_current & 0xFF000000) >> 24);
-  batsol_buf[5] = ((battery_current & 0xFF0000) >> 16);
-  batsol_buf[6] = ((battery_current & 0xFF00) >> 8);
-  batsol_buf[7] = ((battery_current & 0xFF));
-  batsol_buf[8] = ((solar_voltage & 0xFF00) >> 8);
-  batsol_buf[9] = ((solar_voltage & 0xFF));
-  batsol_buf[10] = ((solar_current & 0xFF000000) >> 24);
-  batsol_buf[11] = ((solar_current & 0xFF0000) >> 16);
-  batsol_buf[12] = ((solar_current & 0xFF00) >> 8);
-  batsol_buf[13] = ((solar_current & 0xFF));
-  batsol_buf[14] = battery_percent;
-  batsol_buf[15] = ((battery_energy & 0xFF00) >> 8);
-  batsol_buf[16] = ((battery_energy & 0xFF));
-  batsol_buf[17] = ((battery_full & 0xFF00) >> 8);
-  batsol_buf[18] = ((battery_full & 0xFF));
+  energy_buf[2] = ((battery_voltage & 0xFF00) >> 8);
+  energy_buf[3] = ((battery_voltage & 0xFF));
+  energy_buf[4] = ((battery_current & 0xFF000000) >> 24);
+  energy_buf[5] = ((battery_current & 0xFF0000) >> 16);
+  energy_buf[6] = ((battery_current & 0xFF00) >> 8);
+  energy_buf[7] = ((battery_current & 0xFF));
+  energy_buf[8] = ((solar_voltage & 0xFF00) >> 8);
+  energy_buf[9] = ((solar_voltage & 0xFF));
+  energy_buf[10] = ((solar_current & 0xFF000000) >> 24);
+  energy_buf[11] = ((solar_current & 0xFF0000) >> 16);
+  energy_buf[12] = ((solar_current & 0xFF00) >> 8);
+  energy_buf[13] = ((solar_current & 0xFF));
+  energy_buf[14] = battery_percent;
+  energy_buf[15] = ((battery_energy & 0xFF00) >> 8);
+  energy_buf[16] = ((battery_energy & 0xFF));
+  energy_buf[17] = ((battery_full & 0xFF00) >> 8);
+  energy_buf[18] = ((battery_full & 0xFF));
+
+  signpost_energy_remaining_t rem;
+  rem.controller_energy_remaining = signpost_energy_policy_get_controller_energy_remaining_uwh();
+  for(uint8_t i = 0; i < 8; i++) {
+    rem.module_energy_remaining[i] = signpost_energy_policy_get_module_energy_remaining_uwh(i);
+  }
+
+  //send this data to the radio module
+  energy_buf[19] = (((rem.module_energy_remaining[0]/1000) & 0xFF00) >> 8 );
+  energy_buf[20] = (((rem.module_energy_remaining[0]/1000) & 0xFF));
+  energy_buf[21] = (((rem.module_energy_remaining[1]/1000) & 0xFF00) >> 8 );
+  energy_buf[22] = (((rem.module_energy_remaining[1]/1000) & 0xFF));
+  energy_buf[23] = (((rem.module_energy_remaining[2]/1000) & 0xFF00) >> 8 );
+  energy_buf[24] = (((rem.module_energy_remaining[2]/1000) & 0xFF));
+  energy_buf[25] = (((rem.controller_energy_remaining/1000) & 0xFF00) >> 8 );
+  energy_buf[26] = (((rem.controller_energy_remaining/1000) & 0xFF));
+  energy_buf[27] = 0;
+  energy_buf[28] = 0;
+  energy_buf[29] = (((rem.module_energy_remaining[5]/1000) & 0xFF00) >> 8 );
+  energy_buf[30] = (((rem.module_energy_remaining[5]/1000) & 0xFF));
+  energy_buf[31] = (((rem.module_energy_remaining[6]/1000) & 0xFF00) >> 8 );
+  energy_buf[32] = (((rem.module_energy_remaining[6]/1000) & 0xFF));
+  energy_buf[33] = (((rem.module_energy_remaining[7]/1000) & 0xFF00) >> 8 );
+  energy_buf[34] = (((rem.module_energy_remaining[7]/1000) & 0xFF));
 
   int rc;
-  rc = signpost_networking_send_bytes(ModuleAddressRadio,batsol_buf,19);
-  batsol_buf[1]++;
+  rc = signpost_networking_send_bytes(ModuleAddressRadio,energy_buf,35);
+  energy_buf[1]++;
 
   if(rc >= 0) {
     // Tickle the watchdog because something good happened.
@@ -186,15 +166,8 @@ int main (void) {
   energy_buf[0] = 0x01;
   energy_buf[1] = 0x00;
 
-  energy_av_buf[0] = 0x04;
-  energy_av_buf[1] = 0x00;
-
   gps_buf[0] = 0x02;
   gps_buf[1] = 0x00;
-
-  batsol_buf[0] = 0x03;
-  batsol_buf[1] = 0x00;
-
 
   ////////////////////////////////////////////////
   // Setup watchdog
@@ -203,26 +176,12 @@ int main (void) {
 
   printf("Everything intialized\n");
 
-  uint32_t index = 1;
-  while(1) {
 
-    // get energy updates every 10 seconds
-    if ((index % 60) == 0) {
-      printf("CONTROLLER_STATE: Check energy\n");
-      get_energy_remaining();
-    }
-
-    if((index %60) == 20) {
-      get_batsol();
-    }
-
-    if ((index % 60) == 40) {
-      printf("CONTROLLER_STATE: Check energy\n");
-      get_energy_average();
-    }
-
-    index++;
-    delay_ms(1000);
-  }
+  //setup timers to send status updates to the radio
+  static tock_timer_t energy_send_timer;
+  timer_every(60000, send_energy_update, NULL, &energy_send_timer);
+  delay_ms(30000);
+  static tock_timer_t gps_send_timer;
+  timer_every(60000, send_gps_update, NULL, &gps_send_timer);
 }
 
