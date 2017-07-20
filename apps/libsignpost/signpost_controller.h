45d5b4987c983acf722a05eb31e7a99c6c6bacf3
Josh Adkins <adkinsjd@umich.edu>
2017-07-20 03:22:53 -0700

    added gps data access method

--- apps/libsignpost/signpost_controller.h
+++ apps/libsignpost/signpost_controller.h
@@ -4,7 +4,10 @@
 extern "C" {
 #endif
 
+#include "signpost_api.h"
+
 int signpost_controller_init (void);
+void signpost_controller_get_gps(signpost_timelocation_time_t* time, signpost_timelocation_location_t* location);
 
 #ifdef __cplusplus
 }
