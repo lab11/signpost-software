58dcae51e17e049f4c3b69ea4f88a71844daecdd
Josh Adkins <adkinsjd@umich.edu>
2017-08-01 15:58:06 -0700

    removed changing random memory so app actually runs

--- apps/tests/stfu_test/main.c
+++ apps/tests/stfu_test/main.c
@@ -7,10 +7,10 @@ int main (void) {
 
   printf("started\n");
 
-  uint8_t buffer[4] = {74, 79, 83, 72};
-  ret = signpost_tock_firmware_update_write_buffer(buffer, 0x304, 4);
-  printf("rename %s\n", tock_strerror(ret));
+  //uint8_t buffer[4] = {74, 79, 83, 72};
+  //ret = signpost_tock_firmware_update_write_buffer(buffer, 0x304, 4);
+  //printf("rename %s\n", tock_strerror(ret));
 
-  ret = signpost_tock_firmware_update_go(0x60000, 0x30000, 1024, 0);
+  ret = signpost_tock_firmware_update_go(0x60000, 0x30000, 1024, 0xb13aeefa);
   printf("ok %s\n", tock_strerror(ret));
 }
