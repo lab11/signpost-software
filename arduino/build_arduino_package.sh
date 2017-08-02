acc710931fffa5ca65129a02f07c51eca9ddae55
theomil <thmiller917@gmail.com>
2017-08-02 17:01:38 -0400

    Finished writing shell script for arduino package
    Developer package is still not yet written

--- arduino/build_arduino_package.sh
+++ arduino/build_arduino_package.sh
@@ -4,7 +4,7 @@ PACKAGE_PATH="packages/1.6.15"
 BUILD_PATH="build"
 PACKAGE_BUILD_PATH=${BUILD_PATH}/signpost/samd
 LIBSIGNPOST_PATH="../apps/libsignpost"
-LIBSIGNPOST_BUILD_PATH=${PACKAGE_BUILD_PATH}/libraries/Signpost/
+LIBSIGNPOST_BUILD_PATH=${PACKAGE_BUILD_PATH}/libraries/Signpost
 MBEDTLS_PATH="../apps/support/mbedtls/mbedtls"
 
 set -x
@@ -15,7 +15,7 @@ mkdir ${BUILD_PATH}/signpost
 mkdir ${BUILD_PATH}/signpost/samd
 
 #Copy base samd Arduino hardware package
-cp -r ${PACKAGE_PATH}/* ${BUILD_PATH}
+cp -r ${PACKAGE_PATH}/* ${PACKAGE_BUILD_PATH}
 
 #Copy config files
 cp ${CONFIG_PATH}/platform.txt ${PACKAGE_BUILD_PATH}
@@ -25,12 +25,17 @@ cp ${CONFIG_PATH}/library.properties ${LIBSIGNPOST_BUILD_PATH}
 
 #Copy signpost files
 cp ${LIBSIGNPOST_PATH}/signpost_api.* ${LIBSIGNPOST_BUILD_PATH}/src
-cp ${LIBSIGNPOST_PATH}/signpost_app_layer.* ${LIBSIGNPOST_BUILD_PATH}/src
-cp ${LIBSIGNPOST_PATH}/signpost_protocol_layer.* ${LIBSIGNPOST_BUILD_PATH}/src
-cp ${LIBSIGNPOST_PATH}/signpost_io_interface.* ${LIBSIGNPOST_BUILD_PATH}/src
+cp ${LIBSIGNPOST_PATH}/signbus_app_layer.* ${LIBSIGNPOST_BUILD_PATH}/src
+cp ${LIBSIGNPOST_PATH}/signbus_protocol_layer.* ${LIBSIGNPOST_BUILD_PATH}/src
+cp ${LIBSIGNPOST_PATH}/signbus_io_interface.* ${LIBSIGNPOST_BUILD_PATH}/src
 cp ${LIBSIGNPOST_PATH}/port_signpost.h ${LIBSIGNPOST_BUILD_PATH}/src
 cp ${LIBSIGNPOST_PATH}/port_signpost_arduino.cpp ${LIBSIGNPOST_BUILD_PATH}/src
+cp ${LIBSIGNPOST_PATH}/signpost_entropy.* ${LIBSIGNPOST_BUILD_PATH}/src
 cp ${LIBSIGNPOST_PATH}/CRC16.* ${LIBSIGNPOST_BUILD_PATH}/src
 
 #Copy mbedtls
 cp -r ${MBEDTLS_PATH} ${LIBSIGNPOST_BUILD_PATH}/src
+
+#trim excess mbedtls files
+rm -r ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/yotta/data/example*
+rm -r ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/programs
