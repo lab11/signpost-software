3d90404fc21246bd9a4420876f0e04bad4816ea9
theomil <thmiller917@gmail.com>
2017-08-02 17:09:46 -0400

    Added example file for arduino module

---
+++ apps/arduino_module/module_test/module_test.ino
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+#include <stdint.h>
+#include "signpost_api.h"
+
+#define ARDUINO_MODULE_I2C_ADDRESS 0x48
+
+int rc = -1;
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Program Start\n");
+  rc = signpost_initialization_module_init(ARDUINO_MODULE_I2C_ADDRESS, NULL);
+  while (rc != 0) {
+    char msg[100];
+    sprintf(msg, " - Error initializing bus (code %d). Sleeping for 5s\n", rc);
+    Serial.print(msg);
+    rc = signpost_initialization_module_init(ARDUINO_MODULE_I2C_ADDRESS, NULL);
+    delay(5000);
+  }
+  Serial.print("Intialization Complete\n");
+  delay(1000);
+}
+
+void loop() {
+  signpost_timelocation_time_t t;
+  int result_code = signpost_timelocation_get_time(&t);
+  
+  Serial.print("__________________________________________________________\n");
+  char output[100];
+  //sprintf("%d:%d:%d %d/%d/%d Satellites: %d", output, t.hours, t.minutes, t.seconds,
+                                              //t.month, t.day, t.year, t.satellite_count);
+  //Serial.print(output);
+  Serial.print(t.hours);
+  Serial.print(":");
+  Serial.print(t.minutes);
+  Serial.print(":");
+  Serial.print(t.seconds);
+  Serial.print(" ");
+  Serial.print(t.month);
+  Serial.print("/");
+  Serial.print(t.day);
+  Serial.print("/");
+  Serial.print(t.year);
+  Serial.print(" Satellites: ");
+  Serial.print(t.satellite_count);
+  Serial.print('\n');
+  delay(1000);
+  
+}
