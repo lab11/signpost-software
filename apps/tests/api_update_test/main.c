8aea5ce23a00beb8d645f6885e107fca9bb99d6c
Josh Adkins <adkinsjd@umich.edu>
2017-08-01 23:32:00 -0700

    added example update test that loads blink

---
+++ apps/tests/api_update_test/main.c
@@ -0,0 +1,37 @@
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "timer.h"
+#include "tock.h"
+
+#include "signpost_api.h"
+#include "signbus_io_interface.h"
+
+
+int main(void) {
+    printf("Update Test app\n");
+
+    int rc;
+    do {
+        rc = signpost_initialization_module_init(0x32, SIGNPOST_INITIALIZATION_NO_APIS);
+        if (rc < 0) {
+            printf(" - Error initializing module (code: %d). Sleeping 5s.\n", rc);
+            delay_ms(5000);
+        }
+    } while (rc < 0);
+
+    delay_ms(20000);
+
+    do {
+        rc = signpost_update("cdn.rawgit.com/lab11/signpost-software/init/deployment/audio","0.0.1",0,0x2000,0);
+        if (rc < 0) {
+            printf("Error code %d occurred during update\n", rc);
+            delay_ms(20000);
+        }
+    } while (rc < 0);
+}
