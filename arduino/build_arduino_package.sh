150714dd9be31143cd0ae1759c0bdddb55a8a06c
Theodore Miller <thmiller917@gmail.com>
2017-08-03 13:43:36 -0400

    Fixed bugs in arduino build script
    Added option to build using hardlinks
    instead of copies of files. Fixed
    issue with include path added in platform.txt

--- arduino/build_arduino_package.sh
+++ arduino/build_arduino_package.sh
@@ -6,10 +6,20 @@ PACKAGE_BUILD_PATH=${BUILD_PATH}/signpost/samd
 LIBSIGNPOST_PATH="../apps/libsignpost"
 LIBSIGNPOST_BUILD_PATH=${PACKAGE_BUILD_PATH}/libraries/Signpost
 MBEDTLS_PATH="../apps/support/mbedtls/mbedtls"
+#If developer flag is set, create hard links to files instead of copies
+if [ "$1" = "-d" ]
+then
+	DEVELOPER="--link"
+	echo "Buidling developer package"
+else
+	DEVELOPER=
+	echo "Building consumer package"
+fi
 
 set -x
 #Clear build directory
-rm -r ${BUILD_PATH}/*
+
+rm -rf ${BUILD_PATH}/*
 #Create neccessary directories
 mkdir -p ${PACKAGE_BUILD_PATH}
 
@@ -18,24 +28,22 @@ cp -r ${PACKAGE_PATH}/* ${PACKAGE_BUILD_PATH}/
 
 #Copy config files
 mkdir -p ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${CONFIG_PATH}/platform.txt ${PACKAGE_BUILD_PATH}/
-cp ${CONFIG_PATH}/boards.txt ${PACKAGE_BUILD_PATH}/
-cp ${CONFIG_PATH}/programmers.txt ${PACKAGE_BUILD_PATH}/
-cp ${CONFIG_PATH}/library.properties ${LIBSIGNPOST_BUILD_PATH}/
+cp ${DEVELOPER} ${CONFIG_PATH}/platform.txt ${PACKAGE_BUILD_PATH}/
+cp ${DEVELOPER} ${CONFIG_PATH}/boards.txt ${PACKAGE_BUILD_PATH}/
+cp ${DEVELOPER} ${CONFIG_PATH}/programmers.txt ${PACKAGE_BUILD_PATH}/
+cp ${DEVELOPER} ${CONFIG_PATH}/library.properties ${LIBSIGNPOST_BUILD_PATH}/
 
 #Copy signpost files
-cp ${LIBSIGNPOST_PATH}/signpost_api.* ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/signbus_app_layer.* ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/signbus_protocol_layer.* ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/signbus_io_interface.* ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/port_signpost.h ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/port_signpost_arduino.cpp ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/signpost_entropy.* ${LIBSIGNPOST_BUILD_PATH}/src/
-cp ${LIBSIGNPOST_PATH}/CRC16.* ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signpost_api.* ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signbus_app_layer.* ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signbus_protocol_layer.* ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signbus_io_interface.* ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/port_signpost.h ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/port_signpost_arduino.cpp ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signpost_entropy.* ${LIBSIGNPOST_BUILD_PATH}/src/
+cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/CRC16.* ${LIBSIGNPOST_BUILD_PATH}/src/
 
 #Copy mbedtls
-cp -r ${MBEDTLS_PATH} ${LIBSIGNPOST_BUILD_PATH}/src/
-
-#Trim excess mbedtls files
-rm -r ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/yotta/data/example*
-rm -r ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/programs
+mkdir ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls
+cp -r ${DEVELOPER} ${MBEDTLS_PATH}/library/ ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/
+cp -r ${DEVELOPER} ${MBEDTLS_PATH}/include/ ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/
