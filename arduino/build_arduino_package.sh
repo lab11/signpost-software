7d5d9d34eef04168d81ce7f50c8603c24a2d2669
Theodore Miller <thmiller917@gmail.com>
2017-08-04 14:16:55 -0400

    Finalized Arduino build script
    Script now takes Arduino sketchbook as input and builds package directly there.
    Developer build option creates hardlinks to original files instead of creating copies.

--- arduino/build_arduino_package.sh
+++ arduino/build_arduino_package.sh
@@ -1,39 +1,76 @@
 #!/bin/bash
+#Syntax: ./build_arduino_package [-d] SKETCHBOOK_PATH
+#Adding -d builds a developer package by creating hardlinks to the original files instead of copies
+#SKETCHBOOK_PATH is the path to the sketchbook for your Arduino IDE installation
+#On Linux this is usually ~/Arduino/
+#On Windows this is usually ~/Documents/Arduino/
+
+#Set paths to source files
 CONFIG_PATH="config"
 PACKAGE_PATH="packages/1.6.15"
-BUILD_PATH="build"
-PACKAGE_BUILD_PATH=${BUILD_PATH}/signpost/samd
 LIBSIGNPOST_PATH="../apps/libsignpost"
-LIBSIGNPOST_BUILD_PATH=${PACKAGE_BUILD_PATH}/libraries/Signpost
 MBEDTLS_PATH="../apps/support/mbedtls/mbedtls"
-#If developer flag is set, create hard links to files instead of copies
-if [ "$1" = "-d" ]
-then
-	DEVELOPER="--link"
-	echo "Buidling developer package"
-else
-	DEVELOPER=
-	echo "Building consumer package"
-fi
 
+#Process command line arguments
+case "$#" in
+	"1")
+		if [ ! -d "$1" ]; then
+			echo "${1} is a not a directory, exiting..."
+			exit 1
+		fi
+		SKETCHBOOK_PATH="$1"
+		DEVELOPER=
+		echo "Building consumer package"
+		;;
+	"2")
+		#Check to see which argument is the sketchbook path
+		#If neither of the arguments are directories, exit
+		#If neither of the arguments are "-d", exit
+		if [ -d "$1" ] && [ "$2" = "-d" ] ; then
+			SKETCHBOOK_PATH="$1"
+		elif [ -d "$2" ] && [ "$1" = "-d" ] ; then
+			SKETCHBOOK_PATH="$2"
+		else
+			if [ ! -d "$1" ] && [ ! -d "$2" ] ; then
+				echo "Neither input argument is a directory, exiting..."
+			else
+				echo "Invalid syntax, exiting..."
+			fi
+			exit 1
+		fi
+		DEVELOPER="--link"
+		echo "Buidling developer package"
+		;;
+	*)
+		echo "Invalid number of arguments, exiting..."
+		exit 1
+		;;
+esac
+
+#Set build paths
+PACKAGE_BUILD_PATH=${SKETCHBOOK_PATH}/hardware/signpost/samd
+LIBSIGNPOST_BUILD_PATH=${PACKAGE_BUILD_PATH}/libraries/Signpost
+
+#Echo all commands
 set -x
-#Clear build directory
 
-rm -rf ${BUILD_PATH}/*
+#Clear old package
+rm -rf ${SKETCHBOOK_PATH}hardware/signpost/
+
 #Create neccessary directories
 mkdir -p ${PACKAGE_BUILD_PATH}
 
 #Copy base samd Arduino hardware package
 cp -r ${PACKAGE_PATH}/* ${PACKAGE_BUILD_PATH}/
 
-#Copy config files
+#Copy/link config files
 mkdir -p ${LIBSIGNPOST_BUILD_PATH}/src/
 cp ${DEVELOPER} ${CONFIG_PATH}/platform.txt ${PACKAGE_BUILD_PATH}/
 cp ${DEVELOPER} ${CONFIG_PATH}/boards.txt ${PACKAGE_BUILD_PATH}/
 cp ${DEVELOPER} ${CONFIG_PATH}/programmers.txt ${PACKAGE_BUILD_PATH}/
 cp ${DEVELOPER} ${CONFIG_PATH}/library.properties ${LIBSIGNPOST_BUILD_PATH}/
 
-#Copy signpost files
+#Copy/link signpost files
 cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signpost_api.* ${LIBSIGNPOST_BUILD_PATH}/src/
 cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signbus_app_layer.* ${LIBSIGNPOST_BUILD_PATH}/src/
 cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signbus_protocol_layer.* ${LIBSIGNPOST_BUILD_PATH}/src/
@@ -43,7 +80,7 @@ cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/port_signpost_arduino.cpp ${LIBSIGNPOST_BUIL
 cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/signpost_entropy.* ${LIBSIGNPOST_BUILD_PATH}/src/
 cp ${DEVELOPER} ${LIBSIGNPOST_PATH}/CRC16.* ${LIBSIGNPOST_BUILD_PATH}/src/
 
-#Copy mbedtls
+#Copy/link mbedtls files
 mkdir ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls
 cp -r ${DEVELOPER} ${MBEDTLS_PATH}/library/ ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/
 cp -r ${DEVELOPER} ${MBEDTLS_PATH}/include/ ${LIBSIGNPOST_BUILD_PATH}/src/mbedtls/
