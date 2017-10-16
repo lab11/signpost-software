3ecd31623ed61c8db8da480d0a94f8669b0bff5b
Josh Adkins <adkinsjd@umich.edu>
2017-10-15 22:20:37 -0700

    fixed up the rest of the board files

--- kernel/boards/debug_radio_module/src/main.rs
+++ kernel/boards/debug_radio_module/src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-#![feature(compiler_builtins_lib,const_fn,drop_types_in_const,lang_items)]
+#![feature(compiler_builtins_lib,const_fn,lang_items)]
 
 extern crate capsules;
 extern crate compiler_builtins;
@@ -10,10 +10,8 @@ extern crate kernel;
 extern crate sam4l;
 
 extern crate signpost_drivers;
-extern crate signpost_hil;
 
 use signpost_drivers::gps_console;
-use capsules::timer::TimerDriver;
 use capsules::virtual_alarm::{MuxAlarm, VirtualMuxAlarm};
 use kernel::hil;
 use kernel::hil::Controller;
@@ -45,7 +43,7 @@ static mut PROCESSES: [Option<kernel::Process<'static>>; NUM_PROCS] = [None, Non
 struct DebugRadioModule {
     gps_console: &'static signpost_drivers::gps_console::Console<'static, usart::USART>,
     gpio: &'static capsules::gpio::GPIO<'static, sam4l::gpio::GPIOPin>,
-    timer: &'static TimerDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>>,
+    alarm: &'static capsules::alarm::AlarmDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>>,
     i2c_master_slave: &'static capsules::i2c_master_slave_driver::I2CMasterSlaveDriver<'static>,
     app_watchdog: &'static signpost_drivers::app_watchdog::AppWatchdog<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>>,
     rng: &'static capsules::rng::SimpleRng<'static, sam4l::trng::Trng<'static>>,
@@ -59,17 +57,17 @@ impl Platform for DebugRadioModule {
     {
 
         match driver_num {
-            0 => f(Some(self.gps_console)),
-            1 => f(Some(self.gpio)),
-            3 => f(Some(self.timer)),
-            13 => f(Some(self.i2c_master_slave)),
-            14 => f(Some(self.rng)),
-            30 => f(Some(self.app_flash)),
+            capsules::console::DRIVER_NUM => f(Some(self.gps_console)),
+            capsules::gpio::DRIVER_NUM => f(Some(self.gpio)),
+            capsules::alarm::DRIVER_NUM => f(Some(self.alarm)),
+            capsules::i2c_master_slave_driver::DRIVER_NUM => f(Some(self.i2c_master_slave)),
+            capsules::rng::DRIVER_NUM => f(Some(self.rng)),
+            capsules::app_flash_driver::DRIVER_NUM => f(Some(self.app_flash)),
 
-            108 => f(Some(self.app_watchdog)),
-            109 => f(Some(self.gps_console)),
+            signpost_drivers::app_watchdog::DRIVER_NUM => f(Some(self.app_watchdog)),
+            signpost_drivers::gps_console::DRIVER_NUM => f(Some(self.gps_console)),
 
-            0xff => f(Some(&self.ipc)),
+            kernel::ipc::DRIVER_NUM => f(Some(&self.ipc)),
             _ => f(None)
         }
     }
@@ -116,7 +114,7 @@ pub unsafe fn reset_handler() {
                      115200,
                      &mut gps_console::WRITE_BUF,
                      &mut gps_console::READ_BUF,
-                     kernel::Container::create()));
+                     kernel::Grant::create()));
     hil::uart::UART::set_client(&usart::USART0, gps_console);
 
     //
@@ -132,15 +130,15 @@ pub unsafe fn reset_handler() {
     let virtual_alarm1 = static_init!(
         VirtualMuxAlarm<'static, sam4l::ast::Ast>,
         VirtualMuxAlarm::new(mux_alarm));
-    let timer = static_init!(
-        TimerDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
-        TimerDriver::new(virtual_alarm1, kernel::Container::create()));
-    virtual_alarm1.set_client(timer);
+    let alarm = static_init!(
+        capsules::alarm::AlarmDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
+        capsules::alarm::AlarmDriver::new(virtual_alarm1, kernel::Grant::create()));
+    virtual_alarm1.set_client(alarm);
 
     // Setup RNG
     let rng = static_init!(
             capsules::rng::SimpleRng<'static, sam4l::trng::Trng>,
-            capsules::rng::SimpleRng::new(&sam4l::trng::TRNG, kernel::Container::create()));
+            capsules::rng::SimpleRng::new(&sam4l::trng::TRNG, kernel::Grant::create()));
     sam4l::trng::TRNG.set_client(rng);
 
     // Nonvolatile Pages
@@ -157,7 +155,7 @@ pub unsafe fn reset_handler() {
     let app_flash = static_init!(
         capsules::app_flash_driver::AppFlash<'static>,
         capsules::app_flash_driver::AppFlash::new(nv_to_page,
-            kernel::Container::create(), &mut APP_FLASH_BUFFER));
+            kernel::Grant::create(), &mut APP_FLASH_BUFFER));
 
     //
     // I2C Buses
@@ -228,7 +226,7 @@ pub unsafe fn reset_handler() {
     let module = DebugRadioModule {
         gps_console: gps_console,
         gpio: gpio,
-        timer: timer,
+        alarm: alarm,
         i2c_master_slave: i2c_modules,
         app_watchdog: app_watchdog,
         rng: rng,
--- kernel/boards/microwave_radar_module/src/main.rs
+++ kernel/boards/microwave_radar_module/src/main.rs
@@ -1,7 +1,7 @@
 #![crate_name = "microwave_radar_module"]
 #![no_std]
 #![no_main]
-#![feature(asm,compiler_builtins_lib,const_fn,drop_types_in_const,lang_items)]
+#![feature(asm,compiler_builtins_lib,const_fn,lang_items)]
 
 extern crate capsules;
 extern crate compiler_builtins;
@@ -11,10 +11,8 @@ extern crate kernel;
 extern crate sam4l;
 
 extern crate signpost_drivers;
-extern crate signpost_hil;
 
 //use capsules::console::{self, Console};
-use capsules::timer::TimerDriver;
 use capsules::virtual_alarm::{MuxAlarm, VirtualMuxAlarm};
 use kernel::hil;
 use kernel::hil::Controller;
@@ -47,7 +45,7 @@ struct MicrowaveRadarModule {
     console: &'static capsules::console::Console<'static, usart::USART>,
     gpio: &'static capsules::gpio::GPIO<'static, sam4l::gpio::GPIOPin>,
     led: &'static capsules::led::LED<'static, sam4l::gpio::GPIOPin>,
-    timer: &'static TimerDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>>,
+    alarm: &'static capsules::alarm::AlarmDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>>,
     i2c_master_slave: &'static capsules::i2c_master_slave_driver::I2CMasterSlaveDriver<'static>,
     adc: &'static capsules::adc::Adc<'static, sam4l::adc::Adc>,
     app_watchdog: &'static signpost_drivers::app_watchdog::AppWatchdog<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast<'static>>>,
@@ -65,20 +63,20 @@ impl Platform for MicrowaveRadarModule {
     {
 
         match driver_num {
-            0 => f(Some(self.console)),
-            1 => f(Some(self.gpio)),
-            3 => f(Some(self.timer)),
-            7 => f(Some(self.adc)),
-            8 => f(Some(self.led)),
-            13 => f(Some(self.i2c_master_slave)),
-            14 => f(Some(self.rng)),
-            30 => f(Some(self.app_flash)),
+            capsules::console::DRIVER_NUM => f(Some(self.console)),
+            capsules::gpio::DRIVER_NUM => f(Some(self.gpio)),
+            capsules::alarm::DRIVER_NUM => f(Some(self.alarm)),
+            capsules::adc::DRIVER_NUM => f(Some(self.adc)),
+            capsules::led::DRIVER_NUM => f(Some(self.led)),
+            capsules::i2c_master_slave_driver::DRIVER_NUM => f(Some(self.i2c_master_slave)),
+            capsules::rng::DRIVER_NUM => f(Some(self.rng)),
+            capsules::app_flash_driver::DRIVER_NUM => f(Some(self.app_flash)),
 
-            108 => f(Some(self.app_watchdog)),
-            120 => f(Some(self.stfu)),
-            121 => f(Some(self.stfu_holding)),
+            signpost_drivers::app_watchdog::DRIVER_NUM => f(Some(self.app_watchdog)),
+            signpost_drivers::signpost_tock_firmware_update::DRIVER_NUM => f(Some(self.stfu)),
+            signpost_drivers::signpost_tock_firmware_update::DRIVER_NUM_HOLDING => f(Some(self.stfu_holding)),
 
-            0xff => f(Some(&self.ipc)),
+            kernel::ipc::DRIVER_NUM => f(Some(&self.ipc)),
             _ => f(None)
         }
     }
@@ -143,7 +141,7 @@ pub unsafe fn reset_handler() {
         capsules::console::Console::new(&usart::USART2,
                      115200,
                      &mut capsules::console::WRITE_BUF,
-                     kernel::Container::create()));
+                     kernel::Grant::create()));
     hil::uart::UART::set_client(&usart::USART2, console);
 
     //
@@ -159,15 +157,15 @@ pub unsafe fn reset_handler() {
     let virtual_alarm1 = static_init!(
         VirtualMuxAlarm<'static, sam4l::ast::Ast>,
         VirtualMuxAlarm::new(mux_alarm));
-    let timer = static_init!(
-        TimerDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
-        TimerDriver::new(virtual_alarm1, kernel::Container::create()));
-    virtual_alarm1.set_client(timer);
+    let alarm = static_init!(
+        capsules::alarm::AlarmDriver<'static, VirtualMuxAlarm<'static, sam4l::ast::Ast>>,
+        capsules::alarm::AlarmDriver::new(virtual_alarm1, kernel::Grant::create()));
+    virtual_alarm1.set_client(alarm);
 
     // Setup RNG
     let rng = static_init!(
             capsules::rng::SimpleRng<'static, sam4l::trng::Trng>,
-            capsules::rng::SimpleRng::new(&sam4l::trng::TRNG, kernel::Container::create()));
+            capsules::rng::SimpleRng::new(&sam4l::trng::TRNG, kernel::Grant::create()));
     sam4l::trng::TRNG.set_client(rng);
 
     // Nonvolatile Pages
@@ -184,7 +182,7 @@ pub unsafe fn reset_handler() {
     let app_flash = static_init!(
         capsules::app_flash_driver::AppFlash<'static>,
         capsules::app_flash_driver::AppFlash::new(nv_to_page,
-            kernel::Container::create(), &mut APP_FLASH_BUFFER));
+            kernel::Grant::create(), &mut APP_FLASH_BUFFER));
     hil::nonvolatile_storage::NonvolatileStorage::set_client(nv_to_page, app_flash);
     sam4l::flashcalw::FLASH_CONTROLLER.configure();
 
@@ -320,7 +318,7 @@ pub unsafe fn reset_handler() {
     let stfu_holding = static_init!(
         capsules::nonvolatile_storage_driver::NonvolatileStorage<'static>,
         capsules::nonvolatile_storage_driver::NonvolatileStorage::new(
-            stfu_holding_nv_to_page, kernel::Container::create(),
+            stfu_holding_nv_to_page, kernel::Grant::create(),
             0x60000, // Start address for userspace accessible region
             0x20000, // Length of userspace accessible region
             0,       // Start address of kernel accessible region
@@ -351,7 +349,7 @@ pub unsafe fn reset_handler() {
         console: console,
         gpio: gpio,
         led: led,
-        timer: timer,
+        alarm: alarm,
         i2c_master_slave: i2c_master_slave,
         adc: adc,
         app_watchdog: app_watchdog,
