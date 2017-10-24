24e3ee1fdac8d5d1c0ade03d0a73202f0a926119
Josh Adkins <adkinsjd@umich.edu>
2017-10-24 01:42:48 -0700

    apparently this must be at 48mhz or else it doesn't work

--- kernel/boards/microwave_radar_module/src/main.rs
+++ kernel/boards/microwave_radar_module/src/main.rs
@@ -127,7 +127,7 @@ unsafe fn set_pin_primary_functions() {
 pub unsafe fn reset_handler() {
     sam4l::init();
 
-    sam4l::pm::PM.setup_system_clock(sam4l::pm::SystemClockSource::ExternalOscillator {
+    sam4l::pm::PM.setup_system_clock(sam4l::pm::SystemClockSource::PllExternalOscillatorAt48MHz {
         frequency: sam4l::pm::OscillatorFrequency::Frequency16MHz,
         startup_mode: sam4l::pm::OscillatorStartup::SlowStart,
     });
