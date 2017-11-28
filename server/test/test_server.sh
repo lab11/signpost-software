1fff82ee31a71eb1eda88cf99b1949cbd209dd71
Josh Adkins <adkinsjd@umich.edu>
2017-11-27 21:56:37 -0800

    fixed more of the server run file

--- server/test/test_server.sh
+++ server/test/test_server.sh
@@ -11,14 +11,14 @@ blue=$(tput setaf 4)
 set -e
 
 #start all the mosquitto brokers
-mosquitto -c ./conf/mosquitto/internal.conf &
-mosquitto -c ./conf/mosquitto/lora.conf &
-mosquitto -c ./conf/mosquitto/external.conf &
+mosquitto -c ./conf/mosquitto/internal_conf.d/internal.conf &
+mosquitto -c ./conf/mosquitto/lora_conf.d/lora.conf &
+mosquitto -c ./conf/mosquitto/external_conf.d/external.conf &
 
 #start all of the node services
 node ../uplink/http-receiver/http-receiver.js &
 node ../uplink/lora-receiver/lora-receiver.js ./conf/signpost/uplink/lora-receiver.conf &
-node ../uplink/lora-receiver/metadata-tagger.js ./conf/signpost/uplink/metadata-tagger.conf &
+node ../uplink/metadata-tagger/metadata-tagger.js ./conf/signpost/uplink/metadata-tagger.conf &
 node ../lab11/packet-parser/packet-parser.js ./conf/signpost/lab11/packet-parser.conf &
 
 node ./test-server/test-server.js
